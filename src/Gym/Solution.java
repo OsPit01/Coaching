d0562eabcad6ccc22778a9ac5db7053bfdca20ce
OsPit01 <104819889+OsPit01@users.noreply.github.com>
2022-05-26 21:15:21 +0300

    New

--- src/Gym/Solution.java
+++ src/Gym/Solution.java
@@ -11,13 +11,14 @@ import java.util.List;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        HashMap<String,Integer> login = new HashMap<>();
-        Integer porol;
-        String log;
-
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
+        HashMap<Integer, String> user = new HashMap<>();
+
+        Integer porol;
+        String log;
+        Boolean access = false;
 
         while (true) {
             System.out.println("Hello");
@@ -25,32 +26,51 @@ public class Solution {
             String put = reader.readLine();
 
             if (put.equals("Exit")) {
+                System.out.println("GoodBye");
                 break;
             } else if (!(put.equalsIgnoreCase("REGISTER"))) {
                 System.out.println("Plese inter: \"Register\"" + "   " + " no such command exists");
+            }
 
-            } else if (put.equalsIgnoreCase("REGISTER")) {
+            else if (put.equalsIgnoreCase("REGISTER")) {
                 System.out.println("write your: \"login\"  \nyou can use words and numbers also symbol");
-                 log = reader.readLine();
+                log = reader.readLine();
                 System.out.println("Your login:" + " " + log + "\nYour login added\n");
 
-                System.out.println("Please write your porol" + "only numbers");
+                System.out.println("Please write your porol" + " " + " " + "only numbers\n  \t  size maximum 5 numbers");
+
                 String temp = reader.readLine();
                 porol = Integer.parseInt(temp);
+                int sizePorol = temp.length();
 
-                if(!(porol instanceof Integer)) {
+                if (sizePorol > 6) {
+                    break;
+
+                } else if (!(porol instanceof Integer)) {
+                    break;
+
+                }
+                System.out.println("your porol successfully added");
+                user.put(porol, log);
+                System.out.println("Welcome" + " " + log);
+
+
+                System.out.println("Please confirm your porol");
+
+                while (true) {
+                    if (user.containsKey(Integer.parseInt(reader.readLine()))) {
+                        System.out.println("Welcome");
+                    } else {
+                        System.out.println("not available");
+
+                    }
 
                 }
 
-                System.out.println();
 
             }
-
-
-
         }
 
+
     }
-
-
 }
