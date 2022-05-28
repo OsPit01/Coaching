5d93e342bac65b1a7617f199163c54b6de0e708b
OsPit01 <104819889+OsPit01@users.noreply.github.com>
2022-05-29 02:27:09 +0300

    Finish

--- src/Gym/Solution.java
+++ src/Gym/Solution.java
@@ -11,66 +11,67 @@ import java.util.List;
 public class Solution {
 
     public static void main(String[] args) throws Exception {
-        InputStreamReader input = new InputStreamReader(System.in);
-        BufferedReader reader = new BufferedReader(input);
+        try {
+            InputStreamReader input = new InputStreamReader(System.in);
+            BufferedReader reader = new BufferedReader(input);
 
-        HashMap<Integer, String> user = new HashMap<>();
+            HashMap<Integer, String> user = new HashMap<>();
 
-        Integer porol;
-        String log;
-        Boolean access = false;
+            Integer porol;
+            String log;
 
-        while (true) {
-            System.out.println("Hello");
-            System.out.println("put command: Register");
-            String put = reader.readLine();
 
-            if (put.equals("Exit")) {
-                System.out.println("GoodBye");
-                break;
-            } else if (!(put.equalsIgnoreCase("REGISTER"))) {
-                System.out.println("Plese inter: \"Register\"" + "   " + " no such command exists");
-            }
+            while (true) {
+                System.out.println("Hello");
+                System.out.println("put command: register");
+                String put = reader.readLine();
 
-            else if (put.equalsIgnoreCase("REGISTER")) {
-                System.out.println("write your: \"login\"  \nyou can use words and numbers also symbol");
-                log = reader.readLine();
-                System.out.println("Your login:" + " " + log + "\nYour login added\n");
-
-                System.out.println("Please write your porol" + " " + " " + "only numbers\n  \t  size maximum 5 numbers");
-
-                String temp = reader.readLine();
-                porol = Integer.parseInt(temp);
-                int sizePorol = temp.length();
-
-                if (sizePorol > 6) {
+                if (put.equals("Exit")) {
                     break;
+                } else if (!(put.equals("register"))) {
+                    System.out.println("Plese inter: \"Register\"" + "   " + " no such command exists");
+                } else if (put.equalsIgnoreCase("REGISTER")) {
+                    System.out.println("write your: \"login\"");
+                    log = reader.readLine();
+                    System.out.println("Your login:" + " " + log);
+                    System.out.println(log + " " + "Please write your porol" + " " + " " + "only numbers\n  \t  size maximum 10 numbers");
+                    String temp = reader.readLine();
 
-                } else if (!(porol instanceof Integer)) {
-                    break;
+                    porol = Integer.parseInt(temp);
+                    int sizePorol = temp.length();
 
-                }
-                System.out.println("your porol successfully added");
-                user.put(porol, log);
-                System.out.println("Welcome" + " " + log);
+                    if (sizePorol > 10) {
+                        break;
+
+                    } else if (!(porol instanceof Integer)) {
+                        break;
+
+                    }
+                    System.out.println("your porol successfully added");
+                    user.put(porol, log);
 
 
-                System.out.println("Please confirm your porol");
+                    System.out.println("Please confirm your porol");
 
-                while (true) {
-                    if (user.containsKey(Integer.parseInt(reader.readLine()))) {
-                        System.out.println("Welcome");
-                    } else {
-                        System.out.println("not available");
+                    while (true) {
+                        if (user.containsKey(Integer.parseInt(reader.readLine()))) {
+                            System.out.println("Welcome" + "\t" + log);
+                            break;
+                        } else {
+                            System.out.println("not available\t" + "password entered incorrectly \t " + "please repeat again");
+                        }
 
                     }
 
+
                 }
-
-
+break;
             }
+
+        } catch (Exception e) {
+            System.out.println("password entered incorrectly -" +
+                    " perhaps you have exceeded the size of the numbers or used the wrong format");
         }
-
-
     }
+
 }
